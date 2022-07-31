bbec7e7b8fef65f10204fd466215223d28897304
mehmet <tas349873@gmail.com>
2022-07-31 13:07:57 +0300

    update

--- src/utils/firebase.jsx
+++ src/utils/firebase.jsx
@@ -1,17 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDqWa6JimhKQt6v8VbscB1l6XCWAIAtkGk",
-  authDomain: "fire-contact-meta.firebaseapp.com",
-  databaseURL: "https://fire-contact-meta-default-rtdb.firebaseio.com",
-  projectId: "fire-contact-meta",
-  storageBucket: "fire-contact-meta.appspot.com",
-  messagingSenderId: "324992750204",
-  appId: "1:324992750204:web:00798eb12ae00315996ff9"
+
+
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGİNG_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+ 
 };
 
 // Initialize Firebase
