e171febb6e6e0131449dbcb3ff946bd5f1c457dc
mehmet <tas349873@gmail.com>
2022-07-31 14:09:50 +0300

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import './App.css';
 import Contacts from './components/contacts/Contacts';
 import FormComponent from './components/form/FormComponent';
+import  {AddUser} from "./utils/functions";
+
 
 
 const initialValues={username:"",phoneNumber:"",gender:""}
@@ -10,7 +12,7 @@ function App() {
   const [info, setInfo] = useState(initialValues) 
   const handleSubmit=(e)=>{
     e.preventDefault();
-    AddUser()       
+    AddUser(info)       
     // console.log(info)                                                         
   } 
   return (
--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,6 +1,16 @@
+import { getDatabase ,push,ref, set} from "firebase/database";
+import firebase from "./firebase";
 
 
 
 
-
-export const AddUser
+export const AddUser=(info)=>{
+    const db = getDatabase(firebase);
+    const userRef=ref(db,"users/")
+    const newUserRef=push(userRef)
+    set(newUserRef,{
+        username:info.username,
+        phoneNumber:info.phoneNumber,
+        gender:info.gender
+    })
+}
